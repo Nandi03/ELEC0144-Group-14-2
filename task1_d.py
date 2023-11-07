eaaf2c1b6ae4478e594e515388e21e4e269b64a5
Nandi03 <nchavda260@gmail.com>
2023-11-07 21:43:18 +0000

    layer 1 activation functions completed: ReLu, Linear, Sigmoid, Leaky ReLu

--- task1_d.py
+++ task1_d.py
@@ -36,6 +36,7 @@ epochs = 1000 # increased epochs
 x_train = np.arange(-1, 1, 0.05)
 d_train = 0.8 * x_train**3 + 0.3 * x_train**2 - 0.4 * x_train + np.random.normal(0, 0.02, len(x_train))
 # Training the neural network using stochastic gradient descent
+training_loss = []
 for epoch in range(epochs):
     # Iterate through each training example
     epoch_loss = 0
@@ -57,6 +58,8 @@ for epoch in range(epochs):
 
         l2_weights -= learning_rate * np.outer(l1_output, output_grad)
         l2_bias -= learning_rate * output_grad
+
+        training_loss.append(loss[0])
 
     if epoch % 100 == 0:
         print(f'Epoch: {epoch},  Loss: {loss[0]}')
@@ -130,7 +133,7 @@ epochs = 1000
 # Generate some random training data
 x_train = np.arange(-1, 1, 0.05)
 d_train = 0.8 * x_train**3 + 0.3 * x_train**2 - 0.4 * x_train + np.random.normal(0, 0.02, len(x_train))
-
+training_loss = []
 for epoch in range(epochs):
     # Iterate through each training example
     for i in range(len(x_train)):
@@ -153,7 +156,8 @@ for epoch in range(epochs):
         l2_weights -= learning_rate * np.outer(l1_activated, output_grad)
         l2_bias -= learning_rate * output_grad
 
- 
+        training_loss.append(loss[0])
+
 
     if epoch % 100 == 0:
         print(f'Epoch: {epoch}, Loss: {loss[0]}')
@@ -181,3 +185,181 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# layer 1 = Linear
+# Layer 2 = Linear
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def mse(actual, predicted):
+    return 0.5 * ((predicted - actual) ** 2)
+
+def mse_grad(actual, predicted):
+    return predicted - actual
+
+# Initialize weights randomly
+np.random.seed(0)
+input_size = 1
+hidden_size = 3
+output_size = 1
+
+l1_weights = np.random.randn(input_size, hidden_size) * np.sqrt(2 / (input_size + hidden_size))
+l1_bias = np.zeros((1, hidden_size))  # Bias for hidden layer
+l2_weights = np.random.randn(hidden_size, output_size) * np.sqrt(2 / (hidden_size + output_size))
+l2_bias = np.zeros((1, output_size))  # Bias for output layer
+
+
+# Learning rate
+learning_rate = 0.001
+
+# Number of epochs
+epochs = 1000
+
+# Generate some random training data
+x_train = np.arange(-1, 1, 0.05)
+d_train = 0.8 * x_train**3 + 0.3 * x_train**2 - 0.4 * x_train + np.random.normal(0, 0.02, len(x_train))
+
+training_loss = []
+for epoch in range(epochs):
+    # Iterate through each training example
+    for i in range(len(x_train)):
+        # Forward pass
+        l1_output = np.dot(x_train[i], l1_weights) + l1_bias  # Layer 1
+        output = np.dot(l1_output, l2_weights) + l2_bias  # Layer 2
+
+        # Compute loss
+        loss = mse(d_train[i], output)
+
+        # Back-propagation
+        output_grad = mse_grad(d_train[i], output) 
+        l1_output_grad = np.dot(output_grad, l2_weights.T) 
+
+        # Update weights using gradient descent
+        l1_weights -= learning_rate * np.outer(x_train[i], l1_output_grad)
+        l1_bias -= learning_rate * l1_output_grad
+        
+        l2_weights -= learning_rate * np.outer(l1_output, output_grad)
+        l2_bias -= learning_rate * output_grad
+
+        training_loss.append(loss[0])
+
+
+    if epoch % 100 == 0:
+        print(f'Epoch: {epoch}, Loss: {loss[0]}')
+
+
+x_test = np.arange(-0.97, 0.93, 0.1)
+y_test = 0.8* x_test**3 + 0.3 * x_test**2 - 0.4*x_test + np.random.normal(0, 0.02, len(x_test))
+predictions = []
+for i in range(len(x_test)):
+   # Forward pass
+    l1_output = np.dot(x_test[i], l1_weights) + l1_bias  # Layer 1
+    output = np.sum(np.dot(l1_output, l2_weights) + l2_bias)  # Layer 2
+    predictions.append(output)
+
+
+# Plot the training data, true cubic function, and predictions
+plt.figure(figsize=(8, 6))
+plt.scatter(x_test, y_test, color='blue', label='Training Data')
+plt.plot(x_test, predictions, color='red', marker='o', linestyle='dashed', linewidth=2, markersize=8, label='Predictions')
+plt.xlabel('X_test')
+plt.ylabel('Y')
+plt.title('Cubic Function Approximation using Neural Network (Sequential Learning)')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# layer 1 = Leaky ReLu
+# Layer 2 = Linear
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def leaky_relu(x, alpha=0.1):
+    return np.where(x > 0, x, alpha * x)
+
+def leaky_relu_derivative(x, alpha=0.1):
+    return np.where(x > 0, 1, alpha)
+
+def mse(actual, predicted):
+    return 0.5 * ((predicted - actual) ** 2)
+
+def mse_grad(actual, predicted):
+    return predicted - actual
+
+# Initialize weights randomly
+np.random.seed(0)
+input_size = 1
+hidden_size = 3
+output_size = 1
+
+l1_weights = np.random.randn(input_size, hidden_size) * np.sqrt(2 / (input_size + hidden_size))
+l1_bias = np.zeros((1, hidden_size))  # Bias for hidden layer
+l2_weights = np.random.randn(hidden_size, output_size) * np.sqrt(2 / (hidden_size + output_size))
+l2_bias = np.zeros((1, output_size))  # Bias for output layer
+
+
+# Learning rate
+learning_rate = 0.1
+
+# Number of epochs
+epochs = 1000
+
+# Generate some random training data
+x_train = np.arange(-1, 1, 0.05)
+d_train = 0.8 * x_train**3 + 0.3 * x_train**2 - 0.4 * x_train + np.random.normal(0, 0.02, len(x_train))
+
+training_loss = []
+for epoch in range(epochs):
+    # Iterate through each training example
+    for i in range(len(x_train)):
+        # Forward pass
+        l1_output = np.dot(x_train[i], l1_weights) + l1_bias  # Layer 1
+        l1_activated = leaky_relu(l1_output) # apply Leaky Relu
+        output = np.dot(l1_activated, l2_weights) + l2_bias  # Layer 2
+
+        # Compute loss
+        loss = mse(d_train[i], output)
+
+        # Back-propagation
+        output_grad = mse_grad(d_train[i], output) 
+        l1_output_grad = np.dot(output_grad, l2_weights.T) * leaky_relu_derivative(l1_activated)
+
+        # Update weights using gradient descent
+        l1_weights -= learning_rate * np.outer(x_train[i], l1_output_grad)
+        l1_bias -= learning_rate * l1_output_grad
+        
+        l2_weights -= learning_rate * np.outer(l1_activated, output_grad)
+        l2_bias -= learning_rate * output_grad
+
+        training_loss.append(loss[0])
+
+    if epoch % 100 == 0:
+        print(f'Epoch: {epoch}, Loss: {loss[0]}')
+
+
+x_test = np.arange(-0.97, 0.93, 0.1)
+y_test = 0.8* x_test**3 + 0.3 * x_test**2 - 0.4*x_test + np.random.normal(0, 0.02, len(x_test))
+predictions = []
+for i in range(len(x_test)):
+    # Forward pass
+    l1_output = np.dot(x_test[i], l1_weights) + l1_bias  # Layer 1
+    l1_activated = leaky_relu(l1_output) # apply Leaky Relu
+    output = np.sum(np.dot(l1_activated, l2_weights) + l2_bias ) # Layer 2
+    predictions.append(output)
+
+
+# Plot the training data, true cubic function, and predictions
+plt.figure(figsize=(8, 6))
+plt.scatter(x_test, y_test, color='blue', label='Training Data')
+plt.plot(x_test, predictions, color='red', marker='o', linestyle='dashed', linewidth=2, markersize=8, label='Predictions')
+plt.xlabel('X_test')
+plt.ylabel('Y')
+plt.title('Cubic Function Approximation using Neural Network (Sequential Learning)')
+plt.legend()
+plt.grid(True)
+plt.show()
