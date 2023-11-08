fixed sigmoid activation derivative

test_network.py
@@ -6,7 +6,7 @@ np.random.seed(0)
 model = Model(learning_rate=0.1, optimizer="sgd")
 #model.momentum = 0.3
 
-model.layers.append(Layer("tanh", 1, 3))
+model.layers.append(Layer("sigmoid", 1, 3))
 model.layers.append(Layer("linear", 3, 1))
 
 x_train = np.arange(-1, 1, 0.05)