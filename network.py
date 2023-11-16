b750a46197876471974e2b895b4c261161bc2551
Nandi03 <nchavda260@gmail.com>
2023-11-16 22:33:57 +0000

    fixes

--- network.py
+++ network.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+np.random.seed(0)
 class Model:
     '''
     Implements functions to create and train a neural network.
@@ -305,9 +305,8 @@ class Layer:
         '''
         Initialise a layer with attributes; activation, input_shape and output_shape
         '''
-        np.random.seed(seed)
         self.activation = activation
-        self.weights = np.random.randn(input_shape, output_shape) * np.sqrt(2 / (input_shape + output_shape))
+        self.weights = np.random.randn(input_shape, output_shape) * np.sqrt(2 / (input_shape + output_shape)) 
         self.bias = np.zeros((1, output_shape)) 
         # for adam 
         self.m_W, self.m_B = 0, 0
