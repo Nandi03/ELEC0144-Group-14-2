3b6225c993c4f6265001797a473e7047076649bd
RammBagg <ramit.bag.710@gmail.com>
2023-11-26 14:01:19 +0000

    modifying params

--- task3_d.py
+++ task3_d.py
@@ -1,14 +1,12 @@
 from transfer_learning import TransferLearning
 
-
-
 googlenet_transfer = TransferLearning(
     model_name='googlenet',
     optimiser="adam",
-    batch_size=20,
+    batch_size=30,
     lr=0.0001,
     num_epochs=10,
-    datasetMode="single"
+    datasetMode="multiple"
 
 )
 googlenet_transfer.train()
